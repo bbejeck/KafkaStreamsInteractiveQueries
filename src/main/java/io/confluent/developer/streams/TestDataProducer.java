f94c075da94a53d9229f2d37c306054fad309322
Bill Bejeck <bbejeck@gmail.com>
2023-08-24 17:08:17 -0400

    Add DataFaker dependency Update test data producer generate 200 records every 10MS

--- src/main/java/io/confluent/developer/streams/TestDataProducer.java
+++ src/main/java/io/confluent/developer/streams/TestDataProducer.java
@@ -2,6 +2,10 @@ package io.confluent.developer.streams;
 
 import io.confluent.developer.model.StockTransaction;
 import io.confluent.developer.model.StockTransaction.StockTransactionBuilder;
+import net.datafaker.Faker;
+import net.datafaker.providers.base.Bool;
+import net.datafaker.providers.base.Number;
+import net.datafaker.providers.base.Stock;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -11,10 +15,12 @@ import org.apache.kafka.common.utils.Time;
 
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestDataProducer {
     public static void main(String[] args) throws Exception {
@@ -22,28 +28,35 @@ public class TestDataProducer {
         try(InputStreamReader inputStreamReader = new InputStreamReader( new FileInputStream("src/main/resources/confluent.properties")) ) {
             properties.load(inputStreamReader);
         }
-        Random random = new Random();
         Time time = Time.SYSTEM;
         Serializer<StockTransaction> stockTransactionSerializer = SerdeUtil.stockTransactionSerde().serializer();
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, stockTransactionSerializer.getClass());
+        Faker faker = new Faker();
+        Stock stockFaker = faker.stock();
+        Number numberFaker = faker.number();
+        Bool booleanFaker = faker.bool();
+        List<String> keys = List.of("CFLT", "MSFT", "GOOG", "AACO", "AAPL", "ZELK");
+
+        List<String> baseSymbols = Stream.generate(stockFaker::nsdqSymbol).limit(200).toList();
+        List<String> tickerSymbols = new ArrayList<>(baseSymbols);
+        tickerSymbols.addAll(keys);
+
 
             try (KafkaProducer<String, StockTransaction> producer = new KafkaProducer<>(properties)) {
                 while (true) {
-                    List<String> keys = List.of("CFLT", "MSFT", "GOOG", "AACO", "AAPL", "ZELK");
                     StockTransactionBuilder builder = StockTransactionBuilder.builder();
-                    List<StockTransaction> stockTransactionList = keys.stream().map(key -> builder.withSymbol(key)
-                            .withAmount(random.nextDouble() * random.nextInt(500) * (random .nextBoolean() ? -1 : 1))
-                            .withBuy(random.nextBoolean())
-                            .build()).toList();
-                    stockTransactionList.forEach(transaction -> producer.send(new ProducerRecord<>("input", transaction.getSymbol(), transaction), (meta, e) -> {
+                    tickerSymbols.stream().map(key -> builder.withSymbol(key)
+                            .withAmount(numberFaker.randomDouble(2, 1, 5))
+                            .withBuy(booleanFaker.bool())
+                            .build()).forEach(transaction -> producer.send(new ProducerRecord<>("input", transaction.getSymbol(), transaction), (meta, e) -> {
                         if (e != null) {
                             System.out.printf("Error producing %s %n", e);
                         } else {
                             System.out.printf("Produced record offset=%d, partition=%d, ts=%d %n", meta.offset(), meta.partition(), meta.timestamp());
                         }
-                    }));
-                    time.sleep(1000);
+                        }));
+                    time.sleep(50);
                 }
             }
     }
